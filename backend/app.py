276b07d40040c51fb789bfdcef8e85120271ada3
LucasHolanda23 <lucasrssholanda@gmail.com>
2026-06-10 11:47:47 -0300

    adicionei o front e o back com dockerfile, otimizei python:3.11 para slim e build do front no ci

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,54 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
+import os
 
-from config import Config
-from routes import main_bp
+app = Flask(__name__, template_folder='../frontend')
+app.secret_key = os.getenv('SECRET_KEY', 'chave-secreta-fallback')
 
+USUARIOS = {'admin': 'admin123', 'estudante': 'senha123'}
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    app.register_blueprint(main_bp)
-    return app
+@app.route('/')
+def home():
+    if 'usuario' in session:
+        return render_template('sucesso.html', usuario=session['usuario'])
+    return redirect(url_for('login'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    erro = None
+    if request.method == 'POST':
+        u = request.form.get('username')
+        s = request.form.get('password')
+        
+        if u in USUARIOS and USUARIOS[u] == s:
+            session['usuario'] = u
+            return redirect(url_for('home'))
+        else:
+            erro = 'Usuário ou senha inválidos!'
+            
+    return render_template('index.html', erro=erro)
 
-app = create_app()
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    erro = None
+    if request.method == 'POST':
+        u = request.form.get('username')
+        s = request.form.get('password')
+        
+        if not u or not s:
+            erro = "Usuário e senha são obrigatórios!"
+        else:
+            if u in USUARIOS:
+                erro = 'Este usuário já existe!'
+            else:
+                USUARIOS[u] = s
+                session['usuario'] = u
+                return redirect(url_for('home'))
+    return render_template('cadastro.html', erro=erro)
+
+@app.route('/logout')
+def logout():
+    session.pop('usuario', None)
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
